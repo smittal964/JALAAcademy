4bd04bf16425d86ec67863ec467f44297b508c8f
smittal964 <53136155+smittal964@users.noreply.github.com>
2023-02-05 13:21:01 +0530

    Add files via upload

---
+++ JavaJalaAcademy/src/Inheritance/MyThreeMethods/src/mythreemethods/MyThreeMethods.java
@@ -0,0 +1,38 @@
+
+package mythreemethods;
+
+public class MyThreeMethods {
+   
+ class Computer { 
+  Computer() {
+    System.out.println("Constructor of Computer class.");
+  }
+ 
+  void computer_method() {
+    System.out.println("Power gone! Shut down your PC soon...");
+  }
+ }
+
+    
+    public static void main(String[] args) {
+    Machine my = new Machine();
+    Laptop your = new Laptop();
+   
+    my.computer_method();
+    your.laptop_method();
+  }
+}
+class Laptop {
+  Laptop() {
+    System.out.println("Constructor of Laptop class.");
+  }
+ 
+  void laptop_method() {
+    System.out.println("99% Battery available.");
+   
+        
+        
+        
+    }
+    
+}
